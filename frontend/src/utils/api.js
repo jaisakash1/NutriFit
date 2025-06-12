37c8a2746038409953f05cec48949921c95ff8c9
Akash <akashjais9341@gmail.com>
2025-06-12 20:43:11 +0530

    idkg

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
 
-const API_BASE_URL =  'https://nutrifit-1.onrender.com/api';
+const API_BASE_URL =  'https://nutrifit-4.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
