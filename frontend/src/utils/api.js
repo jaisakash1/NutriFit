9e6d5178de9ab9caf2c6a1e44bfd5c92586df87a
Akash <akashjais9341@gmail.com>
2025-06-13 10:59:14 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
 
-const API_BASE_URL =  'https://nutrifit-4.onrender.com/api';
+const API_BASE_URL =  '';
 // const API_BASE_URL =  'http://localhost:5000/api';
 
 const api = axios.create({
