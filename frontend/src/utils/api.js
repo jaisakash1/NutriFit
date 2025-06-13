5a320d25afb9fcae262967dec8c21d5e4e1becfa
Akash <akashjais9341@gmail.com>
2025-06-13 12:10:45 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
 
-const API_BASE_URL =  '';
+const API_BASE_URL =  process.env.SERVER_URL;
 // const API_BASE_URL =  'http://localhost:5000/api';
 
 const api = axios.create({
