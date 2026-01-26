2232f7c4c7dd83be106aa74862592e54d07289f1
Akash <akashjais9341@gmail.com>
2026-01-26 23:02:29 +0530

    backend now working,there was gemini model error and app password expiry for mail service

--- backend/server.js
+++ backend/server.js
@@ -267,7 +267,10 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Handle preflight (OPTIONS) requests
-app.options('*', cors(corsOptions));
+// app.options('*', cors(corsOptions));
+
+
+app.options('(.*)', cors(corsOptions));
 
 // Rate limiting configuration
 const authLimiter = rateLimit({
